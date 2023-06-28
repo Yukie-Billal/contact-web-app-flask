eff5439bf92b4e86a3a357293e6c96f5658a5a8b
GMNocK <userwith.immortal@gmail.com>
2023-06-28 18:54:25 +0700

    add validator : validate add & update contact

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from . import app
 from . import models
-
+from validator import validate
 
 @app.route('/')
 def index() :
@@ -15,8 +15,23 @@ def create() :
 @app.route('/contact', methods=['GET','POST'])
 def contact() :
     if request.method == "POST" :
-        models.contact.insert(request.form["nama"], request.form["nohp"])
-        return redirect('/contact')
+        nama = request.form["nama"]
+        nohp = request.form["nohp"]
+        request_data = {
+            "nama": nama,
+            "nohp": nohp,
+        }
+
+        rules = {
+            "nama": "required",
+            "nohp": "integer"
+        }
+        result, _, err = validate(request_data, rules, return_info=True)
+        if result :
+            models.contact.insert(nama, nohp)
+            return redirect('/contact')
+        else :
+            return '<h1>403 Bad Request</h1>'
     
     contacts = models.contact.getContact()
     return render_template('contact/index.html', contacts=contacts, update=False, title="Halaman Contact")
@@ -24,12 +39,22 @@ def contact() :
 @app.route('/contact/update/<int:id>', methods=['GET',"POST"])
 def contact_update(id) :
     if request.method == "POST" :
-        form = request.form
-        nama = form["nama"]
-        nohp = form["nohp"]
-        models.contact.update(id,nama,nohp)
-        return redirect('/contact')
-    
+        nama = request.form["nama"]
+        nohp = request.form["nohp"]
+        request_data = {
+            "nama": nama,
+            "nohp": nohp,
+        }
+        rules = {
+            "nama": "required",
+            "nohp": "integer"
+        }
+        result = validate(request_data, rules)
+        if result :
+            models.contact.update(id,nama,nohp)
+            return redirect('/contact')
+        else : 
+            return '<h1>403 Bad Request</h1>'
     contact = models.contact.getContactId(id)
     return render_template('contact/index.html', update=True, contact=contact, title="Ubah Contact")
 
